45fa2bb55d60963745e944943b952431d2f63b70
HallelO <hallelochana@gmail.com>
2025-06-18 22:14:15 +0300

    t

--- unittests/renderer/Miniproject1Test.java
+++ unittests/renderer/Miniproject1Test.java
@@ -7,87 +7,122 @@ import lighting.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Miniproject1Test{
+public class Miniproject1Test {
 
     public static void main(String[] args) {
         Scene scene = new Scene("Mini-project 1: Soft Shadows - Modified");
         scene.setBackground(new Color(10, 10, 30));
-        scene.setAmbientLight(new AmbientLight(new Color(20, 20, 35)));
+        scene.setAmbientLight(new AmbientLight(new Color(40, 40, 60)));
 
-        scene.lights.add(new PointLight(new Color(600, 500, 400), new Point(-80, 120, 200))
-                .setKl(0.0003).setKq(0.0002));
-        scene.lights.add(new DirectionalLight(new Color(80, 120, 160), new Vector(1, -0.8, -1)));
-        scene.lights.add(new SpotLight(new Color(400, 250, 150), new Point(100, 150, 120), new Vector(-0.6, -1, -0.8))
-                .setKl(0.0001).setKq(0.00003));
-        scene.lights.add(new PointLight(new Color(180, 180, 120), new Point(50, 100, -20))
+        // Lights
+        scene.lights.add(new PointLight(new Color(800, 600, 500), new Point(60, 150, 100))
                 .setKl(0.0002).setKq(0.0001));
+        scene.lights.add(new DirectionalLight(new Color(300, 300, 250), new Vector(-1, -1, -0.8)));
 
         List<Intersectable> geometries = new ArrayList<>();
 
+        // Floor and walls
         geometries.add(new Plane(new Point(0, -50, 0), new Vector(0, 1, 0))
-                .setEmission(new Color(50, 60, 40))
-                .setMaterial(new Material().setKD(0.9).setKS(0.2).setShininess(50)));
-        geometries.add(new Plane(new Point(0, 0, -200), new Vector(0, 0, 1))
-                .setEmission(new Color(30, 40, 70))
-                .setMaterial(new Material().setKD(0.8).setKS(0.2).setShininess(30)));
+                .setEmission(new Color(80, 70, 60))
+                .setMaterial(new Material().setKD(0.9).setKS(0.3).setShininess(100).setKR(0.3)));
+        geometries.add(new Plane(new Point(0, 0, -300), new Vector(0, 0, 1))
+                .setEmission(new Color(30, 30, 40))
+                .setMaterial(new Material().setKD(0.6).setKS(0.2).setShininess(60)));
         geometries.add(new Plane(new Point(-150, 0, 0), new Vector(1, 0, 0))
-                .setEmission(new Color(60, 40, 70))
-                .setMaterial(new Material().setKD(0.85).setKS(0.15).setShininess(25)));
+                .setEmission(new Color(60, 50, 70))
+                .setMaterial(new Material().setKD(0.7).setKS(0.3).setShininess(70)));
 
-        geometries.add(new Sphere(new Point(-50, 0, -30), 28)
-                .setEmission(new Color(220, 110, 40))
-                .setMaterial(new Material().setKD(0.5).setKS(0.8).setShininess(150)));
+        // House
+        double baseSize = 60;
+        double baseHeight = 60;
+        double baseY = -50;
+        double baseZ = -100;
+        Color baseColor = new Color(60, 160, 120);
+        Material houseMat = new Material().setKD(0.65).setKS(0.25).setShininess(70).setKR(0.15);
 
-        geometries.add(new Sphere(new Point(40, 20, -110), 30)
-                .setEmission(new Color(40, 70, 200))
-                .setMaterial(new Material().setKD(0.3).setKS(0.7).setKR(0.5).setShininess(200)));
+        Point p1 = new Point(-baseSize, baseY, baseZ - baseSize);
+        Point p2 = new Point(baseSize, baseY, baseZ - baseSize);
+        Point p3 = new Point(baseSize, baseY, baseZ + baseSize);
+        Point p4 = new Point(-baseSize, baseY, baseZ + baseSize);
+        Point p5 = new Point(-baseSize, baseY + baseHeight, baseZ - baseSize);
+        Point p6 = new Point(baseSize, baseY + baseHeight, baseZ - baseSize);
+        Point p7 = new Point(baseSize, baseY + baseHeight, baseZ + baseSize);
+        Point p8 = new Point(-baseSize, baseY + baseHeight, baseZ + baseSize);
 
-        geometries.add(new Sphere(new Point(80, 5, -70), 22)
-                .setEmission(new Color(100, 220, 100))
-                .setMaterial(new Material().setKD(0.2).setKS(0.4).setKT(0.6).setShininess(100)));
+        geometries.add(new Polygon(p1, p2, p3, p4).setEmission(baseColor).setMaterial(houseMat));
+        geometries.add(new Polygon(p5, p6, p7, p8).setEmission(baseColor).setMaterial(houseMat));
+        geometries.add(new Polygon(p1, p2, p6, p5).setEmission(baseColor).setMaterial(houseMat));
+        geometries.add(new Polygon(p2, p3, p7, p6).setEmission(baseColor).setMaterial(houseMat));
+        geometries.add(new Polygon(p3, p4, p8, p7).setEmission(baseColor).setMaterial(houseMat));
+        geometries.add(new Polygon(p4, p1, p5, p8).setEmission(baseColor).setMaterial(houseMat));
 
-        geometries.add(new Triangle(new Point(-120, -30, -60), new Point(-80, -30, -60), new Point(-100, 40, -60))
-                .setEmission(new Color(220, 60, 180))
-                .setMaterial(new Material().setKD(0.7).setKS(0.3).setShininess(50)));
+        // Roof
+        Point roofTop = new Point(0, baseY + baseHeight + 40, baseZ);
+        Color roofColor = new Color(200, 100, 60);
+        Material roofMat = new Material().setKD(0.6).setKS(0.4).setShininess(100);
+        geometries.add(new Triangle(p5, p6, roofTop).setEmission(roofColor).setMaterial(roofMat));
+        geometries.add(new Triangle(p6, p7, roofTop).setEmission(roofColor).setMaterial(roofMat));
+        geometries.add(new Triangle(p7, p8, roofTop).setEmission(roofColor).setMaterial(roofMat));
+        geometries.add(new Triangle(p8, p5, roofTop).setEmission(roofColor).setMaterial(roofMat));
 
-        geometries.add(new Triangle(new Point(90, -30, -50), new Point(130, -30, -50), new Point(110, 25, -50))
-                .setEmission(new Color(100, 200, 100))
-                .setMaterial(new Material().setKD(0.75).setKS(0.25).setShininess(40)));
+        // Cube shape
+        Point c1 = new Point(-90, -50, 20);
+        Point c2 = new Point(-70, -50, 20);
+        Point c3 = new Point(-70, -30, 20);
+        Point c4 = new Point(-90, -30, 20);
+        Point c5 = new Point(-90, -50, 40);
+        Point c6 = new Point(-70, -50, 40);
+        Point c7 = new Point(-70, -30, 40);
+        Point c8 = new Point(-90, -30, 40);
 
-        double radius = 20;
-        Point center = new Point(20, -49, -10);
-        Point[] hexPoints = new Point[6];
-        for (int i = 0; i < 6; i++) {
-            double angle = Math.PI * i / 3;
-            hexPoints[i] = new Point(
-                    center.getX() + radius * Math.cos(angle),
-                    center.getY(),
-                    center.getZ() + radius * Math.sin(angle)
-            );
-        }
-        geometries.add(new Polygon(hexPoints)
+        Color cubeColor = new Color(255, 120, 180);
+        Material cubeMat = new Material().setKD(0.6).setKS(0.3).setShininess(70).setKR(0.2);
+
+        geometries.add(new Polygon(c1, c2, c3, c4).setEmission(cubeColor).setMaterial(cubeMat));
+        geometries.add(new Polygon(c5, c6, c7, c8).setEmission(cubeColor).setMaterial(cubeMat));
+        geometries.add(new Polygon(c1, c2, c6, c5).setEmission(cubeColor).setMaterial(cubeMat));
+        geometries.add(new Polygon(c2, c3, c7, c6).setEmission(cubeColor).setMaterial(cubeMat));
+        geometries.add(new Polygon(c3, c4, c8, c7).setEmission(cubeColor).setMaterial(cubeMat));
+        geometries.add(new Polygon(c4, c1, c5, c8).setEmission(cubeColor).setMaterial(cubeMat));
+
+        // Triangular prism
+        Point t1 = new Point(60, -50, 60);
+        Point t2 = new Point(80, -50, 60);
+        Point t3 = new Point(70, -30, 60);
+        Point t4 = new Point(60, -50, 80);
+        Point t5 = new Point(80, -50, 80);
+        Point t6 = new Point(70, -30, 80);
+        Color triColor = new Color(100, 255, 200);
+        Material triMat = new Material().setKD(0.5).setKS(0.3).setShininess(60).setKT(0.3);
+
+        geometries.add(new Triangle(t1, t2, t3).setEmission(triColor).setMaterial(triMat));
+        geometries.add(new Triangle(t4, t5, t6).setEmission(triColor).setMaterial(triMat));
+        geometries.add(new Polygon(t1, t2, t5, t4).setEmission(triColor).setMaterial(triMat));
+        geometries.add(new Polygon(t2, t3, t6, t5).setEmission(triColor).setMaterial(triMat));
+        geometries.add(new Polygon(t3, t1, t4, t6).setEmission(triColor).setMaterial(triMat));
+
+        // Spheres near the house
+        geometries.add(new Sphere(new Point(-30, -20, -80), 20)
+                .setEmission(new Color(230, 80, 220))
+                .setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100)));
+
+        geometries.add(new Sphere(new Point(30, -20, -90), 25)
+                .setEmission(new Color(220, 240, 255))
+                .setMaterial(new Material().setKD(0.4).setKS(0.5).setKT(0.7).setShininess(200)));
+
+        // Extra two spheres from original version
+        geometries.add(new Sphere(new Point(-100, -20, -90), 20)
+                .setEmission(new Color(250, 100, 160))
+                .setMaterial(new Material().setKD(0.6).setKS(0.4).setShininess(80)));
+
+        geometries.add(new Sphere(new Point(100, -25, -50), 20)
                 .setEmission(new Color(80, 200, 200))
-                .setMaterial(new Material().setKD(0.8).setKS(0.2).setKR(0.1).setShininess(60)));
-
-        double smallRadius = 12;
-        Point smallCenter = new Point(-40, -49, 30);
-        Point[] smallHex = new Point[6];
-        for (int i = 0; i < 6; i++) {
-            double angle = Math.PI * i / 3;
-            smallHex[i] = new Point(
-                    smallCenter.getX() + smallRadius * Math.cos(angle),
-                    smallCenter.getY(),
-                    smallCenter.getZ() + smallRadius * Math.sin(angle)
-            );
-        }
-        geometries.add(new Polygon(smallHex)
-                .setEmission(new Color(240, 240, 80))
-                .setMaterial(new Material().setKD(0.7).setKS(0.3).setShininess(45)));
+                .setMaterial(new Material().setKD(0.4).setKS(0.4).setKT(0.5).setKR(0.4).setShininess(100)));
 
         scene.geometries.add(geometries.toArray(new Intersectable[0]));
 
         Camera camera = Camera.getBuilder()
-                .setLocation(new Point(-100, 100, 250))
+                .setLocation(new Point(-100, 120, 250))
                 .setDirection(new Point(0, 0, -100), new Vector(0, 1, 0))
                 .setVpSize(300, 300)
                 .setVpDistance(200)
